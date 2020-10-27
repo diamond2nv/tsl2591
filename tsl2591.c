d52b679220f90d27ccb6bc78361258fcce4af2cd
Joe Sandom <jgsandom@hotmail.co.uk>
2020-10-27 00:20:19 +0000

    Added register definitions from tsl2591 datasheet v2-04

--- tsl2591.c
+++ tsl2591.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * @file    tsl2591.c
  * @author  Joe Sandom
@@ -5,16 +6,98 @@
  *
  * @brief Device driver for the TAOS TSL2591. This is a very-high sensitivity
  * light-to-digital converter that transforms light intensity into a digital signal.
-*/
+ */
 
 #include <linux/kernel.h>
 #include <linux/i2c.h>
 #include <linux/module.h>
+#include <linux/mutex.h>
 #include <linux/pm_runtime.h>
 #include <linux/iio/iio.h>
 #include <linux/iio/sysfs.h>
 
+/* TSL2591 Register Set */
+#define TSL2591_ENABLE      0x00
+#define TSL2591_CONFIG      0x01
+#define TSL2591_AILTL       0x04
+#define TSL2591_AILTH       0x05
+#define TSL2591_AIHTL       0x06
+#define TSL2591_AIHTH       0x07
+#define TSL2591_NP_AILTL    0x08
+#define TSL2591_NP_AILTH    0x09
+#define TSL2591_NP_AIHTL    0x0A
+#define TSL2591_NP_AIHTH    0x0B
+#define TSL2591_PERSIST     0x0C
+#define TSL2591_PACKAGE_ID  0x11
+#define TSL2591_DEVICE_ID   0x12
+#define TSL2591_STATUS      0x13
+#define TSL2591_C0_DATAL    0x14
+#define TSL2591_C0_DATAH    0x15
+#define TSL2591_C1_DATAL    0x16
+#define TSL2591_C1_DATAH    0x17
+
+/* TSL2591 Command Register Masks */
+#define TSL2591_CMD_NOP             0xA0
+#define TSL2591_CMD_SF_INTSET       0xD4
+#define TSL2591_CMD_SF_CALS_I       0xD5
+#define TSL2591_CMD_SF_CALS_NPI     0xD7
+#define TSL2591_CMD_SF_CNP_ALSI     0xDA
+
+/* TSL2591 Enable Register Masks */
+#define TSL2591_PWR_ON              0x01
+#define TSL2591_ENABLE_ALS          0x02
+#define TSL2591_ENABLE_ALS_INT      0x10
+#define TSL2591_ENABLE_SLEEP_INT    0x40
+#define TSL2591_ENABLE_NP_INT       0x80
+
+/* TSL2591 Control Register Masks */
+#define TSL2591_CTRL_ALS_INTEGRATION_100MS  0x00
+#define TSL2591_CTRL_ALS_INTEGRATION_200MS  0x01
+#define TSL2591_CTRL_ALS_INTEGRATION_300MS  0x02
+#define TSL2591_CTRL_ALS_INTEGRATION_400MS  0x03
+#define TSL2591_CTRL_ALS_INTEGRATION_500MS  0x04
+#define TSL2591_CTRL_ALS_INTEGRATION_600MS  0x05
+#define TSL2591_CTRL_ALS_LOW_GAIN           0x00
+#define TSL2591_CTRL_ALS_MED_GAIN           0x10
+#define TSL2591_CTRL_ALS_HIGH_GAIN          0x20
+#define TSL2591_CTRL_ALS_MAX_GAIN           0x30
+#define TSL2591_CTRL_SYS_RESET              0x80
+
+/* TSL2591 Persist Register Masks */
+#define TSL2591_PRST_ALS_INT_CYCLE_0        0x00
+#define TSL2591_PRST_ALS_INT_CYCLE_ANY      0x01
+#define TSL2591_PRST_ALS_INT_CYCLE_2        0x02
+#define TSL2591_PRST_ALS_INT_CYCLE_3        0x03
+#define TSL2591_PRST_ALS_INT_CYCLE_5        0x04
+#define TSL2591_PRST_ALS_INT_CYCLE_10       0x05
+#define TSL2591_PRST_ALS_INT_CYCLE_15       0x06
+#define TSL2591_PRST_ALS_INT_CYCLE_20       0x07
+#define TSL2591_PRST_ALS_INT_CYCLE_25       0x08
+#define TSL2591_PRST_ALS_INT_CYCLE_30       0x09
+#define TSL2591_PRST_ALS_INT_CYCLE_35       0x0A
+#define TSL2591_PRST_ALS_INT_CYCLE_40       0x0B
+#define TSL2591_PRST_ALS_INT_CYCLE_45       0x0C
+#define TSL2591_PRST_ALS_INT_CYCLE_50       0x0D
+#define TSL2591_PRST_ALS_INT_CYCLE_55       0x0E
+#define TSL2591_PRST_ALS_INT_CYCLE_60       0x0F
+
+/* TSL2591 PID Register Mask */
+#define TSL2591_PACKAGE_ID_MASK 0x30
+
+/* TSL2591 ID Register Mask */
+#define TSL2591_DEVICE_ID_MASK  0xFF
+
+/* TSL2591 Status Register Masks */
+#define TSL2591_STS_ALS_VALID   0x00
+#define TSL2591_STS_ALS_INT     0x10
+#define TSL2591_STS_NPERS_INT   0x20
+
+/* TSL2591 Constant Values */
+#define TSL2591_PACKAGE_ID_VAL  0x00
+#define TSL2591_DEVICE_ID_VAL   0x50
+
 struct tsl2591_chip {
+	struct mutex als_mutex;
 	struct i2c_client *client;
 };
 
@@ -28,7 +111,7 @@ static const struct attribute_group tsl2591_attribute_group = {
 
 /* Multiple channels can be specified - e.g. infrared light vs visible light */
 static const struct iio_chan_spec tsl2591_channels[] = {
-   {
+	{
 		.type = IIO_LIGHT,
 		.modified = 1,
 		.channel2 = IIO_MOD_LIGHT_IR,
@@ -46,22 +129,22 @@ static int tsl2591_read_raw(struct iio_dev *indio_dev,
 			    struct iio_chan_spec const *chan,
 			    int *val, int *val2, long mask)
 {
-   printk("Reading value from device.\n");
+	printk("Reading value from device.\n");
 
-   return 0;
+	return 0;
 }
 
 static int tsl2591_write_raw(struct iio_dev *indio_dev,
 			     struct iio_chan_spec const *chan,
 			     int val, int val2, long mask)
 {
-   printk("Writing value to device.\n");
+	printk("Writing value to device.\n");
 
-   return 0;
+	return 0;
 }
 
 static const struct iio_info tsl2591_info = {
-	.attrs = &tsl2591_attribute_group, /* General purpose device attributes */
+	//.attrs = &tsl2591_attribute_group, /* General purpose device attributes */
 	.read_raw = tsl2591_read_raw, /* Requesting a value from the device */
 	.write_raw = tsl2591_write_raw, /* Writing a value to the device */
 };
@@ -69,73 +152,100 @@ static const struct iio_info tsl2591_info = {
 static int tsl2591_probe(struct i2c_client *client,
 			 const struct i2c_device_id *id)
 {
-   int ret = 0;
-   struct tsl2591_chip *chip;
+	int ret = 0;
+	struct tsl2591_chip *chip;
 	struct iio_dev *indio_dev;
 
-   dev_info(&client->dev, "Start probing device.\n");
+	dev_info(&client->dev, "Start probing device.\n");
 
-   /* This checks whether the i2c adapter supports smbus byte write and read functionality */
-   /* If this functionality is not present, no point continuing because this i2c client driver */
-   /* will not work with the i2c adapter it's attached to */
-   if (!i2c_check_functionality(client->adapter,
-			     I2C_FUNC_SMBUS_BYTE_DATA)) {
-	   dev_err(&client->dev, "%s: i2c smbus byte data functionality is not supported\n",
+	/* This checks whether the i2c adapter supports smbus byte write and read functionality */
+	/* If this functionality is not present, no point continuing because this i2c client driver */
+	/* will not work with the i2c adapter it's attached to */
+	if (!i2c_check_functionality(client->adapter,
+				I2C_FUNC_SMBUS_BYTE_DATA)) {
+		dev_err(&client->dev, "%s: i2c smbus byte data functionality is not supported\n",
 		__func__);
 		return -EOPNOTSUPP;
 	}
 
-   /* Allocate an iio device using the tsl2591 structure */
-   /* devm means memory is managed and free'd automatically */
-   indio_dev = devm_iio_device_alloc(&client->dev, sizeof(*chip));
-	if (!indio_dev) {
+	/* Allocate an iio device using the tsl2591 structure */
+	/* devm means memory is managed and free'd automatically */
+	indio_dev = devm_iio_device_alloc(&client->dev, sizeof(*chip));
+	if (!indio_dev)
 		return -ENOMEM;
-   }
 
-   /* Assign allocated memory iio device to chip */
-   /* Assign parsed i2c client to internal i2c client in tsl2591 structure */
-   chip = iio_priv(indio_dev);
+	/* Assign allocated memory iio device to chip */
+	/* Assign parsed i2c client to internal i2c client in tsl2591 structure */
+	chip = iio_priv(indio_dev);
 	chip->client = client;
 	i2c_set_clientdata(client, indio_dev);
 
-   /* All chip handshaking would go here */
-   /*---------------------------------------*/
+	mutex_init(&chip->als_mutex);
 
-   /* Initialise iio device info */
-   indio_dev->info = &tsl2591_info; /* callbacks and constant info from driver */
+	/* Read the package ID number */
+	ret = i2c_smbus_read_byte_data(client,
+				       TSL2591_CMD_NOP | TSL2591_PACKAGE_ID);
+
+	if (ret < 0) {
+		dev_err(&client->dev,
+			"%s: failed to read the package ID register\n", __func__);
+		return ret;
+	}
+
+	if ((ret & TSL2591_PACKAGE_ID_MASK) != TSL2591_PACKAGE_ID_VAL) {
+		dev_err(&client->dev, "%s: received an unknown package ID %x\n",
+			__func__, ret);
+		return -EINVAL;
+	}
+
+	/* Read the device ID number */
+	ret = i2c_smbus_read_byte_data(client,
+				       TSL2591_CMD_NOP | TSL2591_DEVICE_ID);
+
+	if (ret < 0) {
+		dev_err(&client->dev,
+			"%s: failed to read the device ID register\n", __func__);
+		return ret;
+	}
+
+	if ((ret & TSL2591_DEVICE_ID_MASK) != TSL2591_DEVICE_ID_VAL) {
+		dev_err(&client->dev, "%s: received an unknown device ID %x\n",
+			__func__, ret);
+		return -EINVAL;
+	}
+
+	/* Initialise iio device info */
+	indio_dev->info = &tsl2591_info; /* callbacks and constant info from driver */
 	indio_dev->channels = tsl2591_channels; /* channel spec table */
 	indio_dev->num_channels = ARRAY_SIZE(tsl2591_channels); /* num channels */
 	indio_dev->modes = INDIO_DIRECT_MODE; /* operating modes supported by device */
 	indio_dev->name = chip->client->name; /* name of device */
 
-   /* Add power management handling here */
-   /* --------------------------------------*/
+	/* Add power management handling here */
+	/* --------------------------------------*/
 
-   if (!indio_dev) {
-		return -ENOMEM;
-   }
-   /* Register the device on the iio framework */
-   ret = devm_iio_device_register(indio_dev->dev.parent, indio_dev);
+	/* Register the device on the iio framework */
+	ret = devm_iio_device_register(indio_dev->dev.parent, indio_dev);
 	if (ret) {
 		dev_err(&client->dev, "%s: iio registration failed\n",
 			__func__);
 		return ret;
 	}
 
-   dev_info(&client->dev, "Probe complete - Light sensor found.\n");
+	dev_info(&client->dev, "Probe complete - Light sensor found.\n");
 
 	return 0;
 }
 
 static int tsl2591_remove(struct i2c_client *client)
 {
-   struct iio_dev *indio_dev = i2c_get_clientdata(client);
+	struct iio_dev *indio_dev = i2c_get_clientdata(client);
 
-   dev_info(&client->dev, "Removing device.\n");
+	dev_info(&client->dev, "Removing device.\n");
 
 	iio_device_unregister(indio_dev);
 
-   return 0;
+	return 0;
 }
 
 static int __maybe_unused tsl2591_suspend(struct device *dev)
